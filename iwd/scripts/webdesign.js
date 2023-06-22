bd1a9cf8ffd835c054e3167a3be27b093163fca5
Heitor Cazado <heitorcazado@gmail.com>
2023-06-22 11:17:20 -0300

    iwd - css add project var and clean some def vals

--- iwd/scripts/webdesign.js
+++ iwd/scripts/webdesign.js
@@ -2,21 +2,16 @@ var index = 0;
 
 document.addEventListener(onload, slideShow());
 
-function slideShow(){
-    
+function slideShow(){ 
     let carouselItems = document.getElementsByTagName("li");
     for(let i = 0; i < carouselItems.length; i++){
         if(i != index)
         carouselItems[i].style.display = "none";
-        console.log("set display none i: "+i);
     }
-
     if(index >= carouselItems.length){
         index = 0;
-        console.log("reseting index variable:"+index);
     }
     carouselItems[index].style.display = "block";
-    console.log("defined property display block in element: "+index);
     index++;
-    setTimeout(slideShow, 10000);
+    setTimeout(slideShow, 8000);
 }
